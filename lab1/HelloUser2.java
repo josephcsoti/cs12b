7b5dd6ed773f1e51e46f7bdfcdb5c4090b36e9f2
Joseph Csoti <josephcsoti@gmail.com>
2018-01-22 14:56:15 -0800

    Lab 1 uploaded

---
+++ lab1/HelloUser2.java
@@ -0,0 +1,16 @@
+/*------------------------------------------------
+File Name: HelloUser2.java
+Desc: Prints out a message to the user
+Instructions: Run using makefile (perferably)
+
+Name:   Joseph Csoti
+CruzID: 1617438
+Class:  CMPS 12M
+Date:   01/16/18
+------------------------------------------------*/
+
+class HelloUser2 {
+  public static void main(String[] args){
+    System.out.println("I'm sorry Dave, I'm afraid I can't do that. Version 2");
+  }
+}
